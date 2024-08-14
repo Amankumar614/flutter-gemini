b662cc64bbcfc41aa1ccaeb881bef847f3bb39d1
aman-kumar-bbi <aman.kumar@media4u.com>
2024-08-14 20:39:45 +0530

    flutter gemini

---
+++ lib/api/api_service.dart
@@ -0,0 +1,5 @@
+class ApiService {
+  static const apiKey =
+      'AIzaSyCp99taLfzz-Tga0Nx7gJncWEa8cngiM1k'; // Add your API key here
+  // Its not a good practice to store API key in the code on production
+}
